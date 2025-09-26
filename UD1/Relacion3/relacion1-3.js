5beaa0e490bd6712bfe614d62edbb8a4504f78a2
Diana Alejandra Radu Giju <dgijrad2710@g.educaand.es>
2025-09-26 13:08:47 +0200

    Repasar la cuatro porque no me funciona. T2 R1 Hasta el ej.5

--- UD1/Relacion3/relacion1-3.js
+++ UD1/Relacion3/relacion1-3.js
@@ -1,9 +1,21 @@
+/**
+ * Calcula tu edad
+ * @param {*} birth Tu año de nacimiento
+ * @returns Tu edad actual aproximada
+ */
 function ageCalculator(birth) {
     let currentYear = 2025;
     let edad = currentYear-birth;
     let mensaje = "They are either " + (edad-1) + " or " + edad;
     return mensaje;
 }
+/**
+ * Calcula la comida que vas a necesitar de por vida
+ * @param {*} age Tu edad actual
+ * @param {*} maxAge La edad máxima estandar
+ * @param {*} amount Cantidad de comida diaria
+ * @returns Cuanta comida vas a necesitar de por vida
+ */
 
 function supplyCalculator(age, maxAge, amount) {
     let tiempo = maxAge-age;
@@ -11,7 +23,11 @@ function supplyCalculator(age, maxAge, amount) {
     let resultado = dias * amount;
     return resultado  
 }
-
+/**
+ * Calcula la circunferencia de un circulo con su radio
+ * @param {*} radio Introduce el radio del circulo
+ * @returns La circunferencia del circulo
+ */
 function circumference(radio) {
     const pi=3.14159;
     return 2*pi*radio;
