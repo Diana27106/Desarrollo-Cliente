7e64064a2b92f7b1d14c15a3105e065f2ffd431d
Diana <dgijrad2710@g.educaand.es>
2026-01-14 13:25:43 +0100

    ok

---
+++ UD7/app1/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Card.module.css";
+
+export default function Card({ producto }) {
+  // console.log(props);
+  return (
+    <div>
+      <h3 className={styles.h3}>{producto.title}</h3>
+      <p>
+        Precio:
+        {producto.price > 100 ? producto.price + " oferta..." : producto.price}
+        {producto.price > 100 && "✅"}
+      </p>
+      <p>{producto.category}</p>
+    </div>
+  );
+}
---
+++ UD7/app1/src/pages/Pruebas.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GridProductos from "../components/GridProductos";
+
+export default function Pruebas() {
+  return (
+    <div>
+      <GridProductos />
+    </div>
+  );
+}
